fd6746c4cc3aca6fec7abc05e592dcdc366a1ac7
vrhidayat <fadli.hidayat.nur@gmail.com>
2025-09-17 18:34:39 +0700

    minor fix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === "production";
 const isVercel = !!process.env.VERCEL;
 
 const nextConfig = {
-    basePath: isProd && !isVercel ? "" : "",
-    assetPrefix: isProd && !isVercel ? "" : "",
+    basePath: isProd && !isVercel ? "/vrhidayat.github.io" : "",
+    assetPrefix: isProd && !isVercel ? "/vrhidayat.github.io/" : "",
     output: "export",
     reactStrictMode: true,
     images: {
