fdffa90d8bf48620734a27137c37c1937a4f9086
vrhidayat <fadli.hidayat.nur@gmail.com>
2025-09-17 18:11:40 +0700

    missing image fix

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,6 @@
 const getImagePrefix = () => {
   return process.env.NODE_ENV === "production"
-    ? "/Next.js-Tailwind-CSS-Portfolio-Template/": "";
+    ? "/vrhidayat.github.io/": "";
 };
 
 export { getImagePrefix };
- 
