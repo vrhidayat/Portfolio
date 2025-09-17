0f6d6fa86eb5f1f2e5a1be587aac543c612a8e61
vrhidayat <fadli.hidayat.nur@gmail.com>
2025-09-17 11:26:47 +0700

    Updates

---
+++ _next/static/chunks/pages/_app-c844540e835ee623.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{2332:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(4198)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[179],function(){return _(2332),_(7268)}),_N_E=n.O()}]);
