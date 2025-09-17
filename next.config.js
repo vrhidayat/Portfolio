fa8bc56cfa7266fa4862fc9aef7a3ef64d252e5c
vrhidayat <fadli.hidayat.nur@gmail.com>
2025-09-17 11:37:35 +0700

    minor updates

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-    basePath: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template" : "",
-    assetPrefix: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template/" : "",
+    basePath: "",
+    assetPrefix: "",
     output: "export",
     reactStrictMode: true,
     images: {
