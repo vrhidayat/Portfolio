a7e9fafa090dec034cefbb36d584ef6eee0382cc
vrhidayat <fadli.hidayat.nur@gmail.com>
2025-09-17 18:52:26 +0700

    experiment

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
 const getImagePrefix = () => {
   return process.env.NODE_ENV === "production"
-    ? "/vrhidayat.github.io/public/": "";
+    ? "/vrhidayat.github.io/": "";
 };
 
 export { getImagePrefix };
